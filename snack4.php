1480e3ed7560f0211aed656889d8084bb3a8f9a2
Dandav2923 <daniele.emili23@gmail.com>
2022-01-31 16:38:10 +0100

    snack 4

--- snack4.php
+++ snack4.php
@@ -5,11 +5,12 @@ Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il pa
 
 
 <?php 
+$paragraph = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit impedit ipsa earum minima repellendus dolor accusamus sunt reprehenderit corporis. Nulla neque unde hic deleniti animi! Qui eligendi autem illum ullam.
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem soluta debitis molestiae voluptatum nisi facere commodi maxime est doloremque. Temporibus inventore modi ab officiis tempore vero error sunt alias odio!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus dolorum perspiciatis labore illo! Facilis, saepe earum quisquam non repellendus reiciendis. Ad esse, amet ipsam corrupti in hic sit recusandae sint.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam repudiandae alias deserunt id ducimus porro, atque eos hic culpa quod minima ex adipisci illo vitae magni asperiores unde numquam. Quos?Lorem ipsum dolor sit amet consectetur adipisicing elit. At accusantium aperiam, non illum adipisci assumenda quos, architecto nostrum sunt fugit reiciendis omnis aliquam numquam, odio doloribus distinctio voluptas tenetur ab.';
 
-
-
-
-
-
+$keywords = preg_split('/[\.]+/', $paragraph);
+print_r($keywords);
 
 ?>
