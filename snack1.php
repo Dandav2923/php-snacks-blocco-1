9a10a4c4e77b22323ec8a004237544426a190213
Dandav2923 <daniele.emili23@gmail.com>
2022-01-31 15:24:29 +0100

    first snack

--- snack1.php
+++ snack1.php
@@ -7,8 +7,23 @@ $name = $_GET["name"];
 $mail = $_GET["mail"];
 $age = $_GET["age"];
 
-
-
-
-
+if (strlen($name) > 3 && strpos($mail, '@') && strpos($mail, '.') && is_numeric($age)){
+    $message = 'accesso riuscito';
+}
+else {
+    $message = ' accesso negato';
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo $message ?></h1>
+</body>
+</html>
